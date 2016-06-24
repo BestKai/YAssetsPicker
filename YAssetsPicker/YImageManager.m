773ce9fa3dab4f7ba856cb85068979ee5b221625
BestKai <best_kai@126.com>
2016-06-24 11:10:29 +0800

    version 0.0.5 图片不显示修复，代理方法优化

--- YAssetsPicker/YImageManager.m
+++ YAssetsPicker/YImageManager.m
@@ -75,7 +75,7 @@
     if (IOS9Later) {
         PHFetchOptions *option = [[PHFetchOptions alloc] init];
         
-        option.predicate = [NSPredicate predicateWithFormat:@"mediaType == %zd", PHAssetMediaTypeImage];
+        option.predicate = [NSPredicate predicateWithFormat:@"mediaType == %d", PHAssetMediaTypeImage];
         option.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:YES]];
         
         PHFetchResult *smartAlbums = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeSmartAlbumUserLibrary options:nil];
